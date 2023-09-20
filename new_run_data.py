11e53400e0257b61a5374a786008956892b445b9
ding mingrui <326847768@qq.com>
2023-09-20 23:25:54 +0800

    将取餐和送餐节点都加入了时间窗，更改了数据结构和读取数据的文件，约束敲的可能还有点问题，模型总是不可行，明天早上来了再检查

---
+++ new_run_data.py
@@ -0,0 +1,94 @@
+import random
+
+
+class PickupNode:
+    def __init__(self, num, cor_x, cor_y, demand, early_time, late_time, service_time, delivery_node_num):
+        self.num = num
+        self.cor_x = cor_x
+        self.cor_y = cor_y
+        self.demand = demand
+        self.early_time = early_time
+        self.late_time = late_time
+        self.service_time = service_time
+        self.delivery_node_num = delivery_node_num
+
+
+class DeliveryNode:
+    def __init__(self, num, cor_x, cor_y, demand, early_time, late_time, service_time, pickup_node_num,
+                 cost_time, restaurant, penalty):
+        self.num = num
+        self.cor_x = cor_x
+        self.cor_y = cor_y
+        self.demand = demand
+        self.early_time = early_time
+        self.late_time = late_time
+        self.service_time = service_time
+        self.pickup_node_num = pickup_node_num
+        self.cost_time = cost_time
+        self.restaurant = restaurant
+        self.penalty = penalty
+
+
+class DepotNode:
+    def __init__(self, num, cor_x, cor_y, early_time, late_time):
+        self.num = num
+        self.cor_x = cor_x
+        self.cor_y = cor_y
+        self.early_time = early_time
+        self.late_time = late_time
+
+
+def read_data(data_path):
+    """
+    num0	X1	Y2	demand3	early_time4	late_time5	service_time6	pickup7	delivery8
+    :param data_path: 读取文件路径
+    :return: depot, pickup nodes, delivery nodes
+    """
+    request_num = 1
+    with open(data_path) as origin_data_file:
+        lines = origin_data_file.readlines()
+        data = [[int(i) for i in line.split()] for line in lines[1:]]
+        depot = DepotNode(data[0][0], data[0][1], data[0][2], data[0][4], data[0][5])
+        requests = data[1:]
+        random.shuffle(requests)    # 随机打乱数据的顺序
+        pickup_nodes = []
+        delivery_nodes = []
+        for line in requests:
+            if line[8] > 0:
+                pickup_nodes.append(PickupNode(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[8]))
+                # 寻找对应的送餐节点
+                for line_inner in data:
+                    if line_inner[0] == line[8]:
+                        # 计算成本的最晚时间和右时间窗一致，所属餐馆和惩罚成本暂时都设为0
+                        delivery_nodes.append(DeliveryNode(line_inner[0], line_inner[1], line_inner[2], line_inner[3],
+                                                           line_inner[4], line_inner[5],line_inner[6], line_inner[7],
+                                                           line_inner[5], 0, 0))
+                        break
+                if len(pickup_nodes) > request_num:  # 随机取出data_size对取送货节点
+                    break
+
+    folder_path = r'C:\Users\32684\Desktop\mdp_data\LL_dataset'
+    depot_path = r'C:\Users\32684\Desktop\mdp_data\LL_dataset\lc_101_depot_' + str(request_num) + '.txt'
+    pickup_path = r'C:\Users\32684\Desktop\mdp_data\LL_dataset\lc_101_pickup_' + str(request_num) + '.txt'
+    delivery_path = r'C:\Users\32684\Desktop\mdp_data\LL_dataset\lc_101_delivery_' + str(request_num) + '.txt'
+    with open(depot_path, 'w') as depot_file:
+        print('num', 'X', 'Y', 'e', 'l', sep='\t', file=depot_file)
+        print(depot.num, depot.cor_x, depot.cor_y, depot.early_time, depot.late_time, sep='\t', file=depot_file)
+    with open(pickup_path, 'w') as pickup_file:
+        print('num', 'X', 'Y', 'demand', 'e', 'l', 'service_time', 'delivery', sep='\t', file=pickup_file)
+        for pickup_node in pickup_nodes:
+            print(pickup_node.num, pickup_node.cor_x, pickup_node.cor_y, pickup_node.demand,
+                  pickup_node.early_time, pickup_node.late_time, pickup_node.service_time,
+                  pickup_node.delivery_node_num, sep='\t', file=pickup_file)
+    with open(delivery_path, 'w') as delivery_file:
+        print('num', 'X', 'Y', 'demand', 'e', 'l', 'service_time', 'pickup', 'cost_time', 'restaurant',
+              'penalty', sep='\t', file=delivery_file)
+        for delivery_node in delivery_nodes:
+            print(delivery_node.num, delivery_node.cor_x, delivery_node.cor_y, delivery_node.demand,
+                  delivery_node.early_time, delivery_node.late_time, delivery_node.service_time,
+                  delivery_node.pickup_node_num, delivery_node.cost_time, delivery_node.restaurant,
+                  delivery_node.penalty, sep='\t', file=delivery_file)
+
+    return depot, pickup_nodes, delivery_nodes
+
+
--- solver_solution.py
+++ solver_solution.py
@@ -1,16 +1,15 @@
 from gurobipy import *
-from run_data import *
+from new_run_data import *
 from draw_picture import Draw
 
 
-[orders, restaurants] = read_data(r'C:\Users\32684\Desktop\mdp_data\test\order1.txt',
-                                  r'C:\Users\32684\Desktop\mdp_data\test\restaurant1.txt')
-order_num = len(orders)     # 订单的数量
-rider_num = 2   # 可用的骑手数量
-max_order = 5   # 每个骑手最多配送的订单数量
-speed = 400     # 骑手行驶的速度 米/分钟
+[depot_node, pickup_nodes, delivery_nodes] = read_data(r'D:\资料\赵秋红导师组\本科毕设\文献\外卖配送\Li and Lim pdp_100\lc101.txt')
+order_num = len(pickup_nodes)     # 订单的数量
+rider_num = 12   # 可用的骑手数量
+max_order = 10   # 每个骑手最多配送的订单数量
+speed = 1     # 骑手行驶的速度 米/分钟
 f_cost = 1      # 骑手行驶的单位距离成本
-tolerance_time = 50     # 最晚配送时间
+
 
 set_P = [i for i in range(1, order_num + 1)]    # 集合P
 set_D = [i for i in range(order_num + 1, 2 * order_num + 1)]    # 集合D
@@ -18,16 +17,15 @@ set_N = [i for i in range(2 * order_num + 2)]   # 集合N
 set_K = [k for k in range(rider_num)]
 start_node = 0
 dest_node = 2 * order_num + 1
-# service_time = [2]    # 每个订单顾客节点的服务时间
-# for j in set_N[1:-1]:
-#     service_time.append(2)
-# service_time.append(0)
-service_time = 2
 
+A_i = {}    # 订单i的可取餐时间
+B_i = {}    # 订单i的最晚取餐时间
+E_i = {}    # 订单i的最早送餐时间
+F_i_hard = {}   # 订单i的最晚送餐时间
+F_i_soft = {}   # 订单i的延迟成本开始计算时间
 s_i = {}    # 餐馆i的单位延迟时间惩罚成本
-l_i = {}    # 餐馆i的配送时效要求
-B_i = {}    # 订单i的可取餐时间
-depot = [1000, 1000]    # 配送中心的坐标
+service_time = {}   # 所有取餐和送餐节点的服务时间
+depot = [depot_node.cor_x, depot_node.cor_y]    # 配送中心的坐标
 node_cor_x = {}     # 记录所有节点的横坐标
 node_cor_y = {}     # 记录所有节点的纵坐标
 node_cor_x[0] = depot[0]
@@ -37,14 +35,20 @@ node_cor_y[2 * order_num + 1] = depot[1]
 big_M = 10000
 
 for i in set_P:
-    B_i[i] = orders[i - 1].ready_time
-    node_cor_x[i] = orders[i - 1].cor_x
-    node_cor_y[i] = orders[i - 1].cor_y
-    cur_res = restaurants[orders[i - 1].restaurant - 1]
-    s_i[i] = cur_res.penalty
-    l_i[i] = cur_res.time
-    node_cor_x[i + order_num] = cur_res.cor_x
-    node_cor_y[i + order_num] = cur_res.cor_y
+    A_i[i] = pickup_nodes[i - 1].early_time
+    B_i[i] = pickup_nodes[i - 1].late_time
+    E_i[i + order_num] = delivery_nodes[i - 1].early_time
+    F_i_hard[i + order_num] = delivery_nodes[i - 1].late_time
+    F_i_soft[i + order_num] = delivery_nodes[i - 1].cost_time
+    s_i[i] = delivery_nodes[i - 1].penalty
+    service_time[i] = pickup_nodes[i - 1].service_time
+    service_time[i + order_num] = delivery_nodes[i - 1].service_time
+    node_cor_x[i] = pickup_nodes[i - 1].cor_x
+    node_cor_y[i] = pickup_nodes[i - 1].cor_y
+    node_cor_x[i + order_num] = delivery_nodes[i - 1].cor_x
+    node_cor_y[i + order_num] = delivery_nodes[i - 1].cor_y
+service_time[start_node] = 0
+service_time[dest_node] = 0
 
 d_i_j = {}  # 记录各节点之间的距离
 t_i_j = {}  # 记录各节点之间的行驶时间
@@ -68,7 +72,6 @@ model = Model('origin_model')
 
 X = {}  # x_i_j_k
 Q = {}  # Q_i_k
-# A = {}  # A_i_k
 L = {}  # L_i_k
 D = {}  # D_i_k
 
@@ -82,11 +85,6 @@ for i in set_N:
     for k in set_K:
         name_Q = 'Q_' + str(i) + '_' + str(k)
         Q[i, k] = model.addVar(0, vtype=GRB.CONTINUOUS, name=name_Q)
-
-# for i in set_N:
-#     for k in set_K:
-#         name_A = 'A_' + str(i) + '_' + str(k)
-#         A[i, k] = model.addVar(0, vtype=GRB.CONTINUOUS, name=name_A)
 
 for i in set_N:
     for k in set_K:
@@ -159,46 +157,56 @@ for i in set_P + set_D:
             expr2.addTerms(1, X[i, j, k])
         model.addConstr(expr1 - expr2 == 0, 'c9')
 
-# c13
+# c13  -1
 for i in set_P:
     for k in set_K:
-        model.addConstr(L[i, k] >= B_i[i], 'c13')
+        model.addConstr(L[i, k] <= B_i[i], 'c13')
 
-# c13--
+# c13  -2
 for i in set_P:
     for k in set_K:
-        model.addConstr(L[i + order_num, k] <= l_i[i] + tolerance_time + B_i[i], 'c13--')
+        model.addConstr(L[i, k] >= A_i[i], 'c13')
 
-# c14
-for i in set_P:
+# c14  -1
+for i in set_D:
     for k in set_K:
-        model.addConstr(L[i, k] + t_i_j[i, i + order_num] <= L[i + order_num, k], 'c14')
+        model.addConstr(L[i, k] <= F_i_hard[i], 'c14')
+
+# c14  -2
+for i in set_D:
+    for k in set_K:
+        model.addConstr(L[i, k] >= E_i[i], 'c14')
 
 # c15
-for k in set_K:
-    model.addConstr(L[start_node, k] <= L[dest_node, k] - service_time, 'c15')
-
-# c16
-for i in set_N:
-    for k in set_K:
-        model.addConstr(Q[i, k] <= max_order * 2, 'c16')
-
-# c18
 for i in set_P:
     for k in set_K:
-        model.addConstr(D[i + order_num, k] >= L[i + order_num, k] - B_i[i] - l_i[i], 'c18')
+        model.addConstr(L[i, k] + t_i_j[i, i + order_num] <= L[i + order_num, k], 'c15')
 
-# c20
+# c16
+for k in set_K:
+    model.addConstr(L[start_node, k] <= L[dest_node, k], 'c16')
+
+# c17
+for i in set_N:
+    for k in set_K:
+        model.addConstr(Q[i, k] <= max_order * 2, 'c17')
+
+# c19
+for i in set_P:
+    for k in set_K:
+        model.addConstr(D[i + order_num, k] >= L[i + order_num, k] - F_i_soft[i + order_num], 'c19')
+
+# c21
 for i in set_N:
     for j in set_N:
         for k in set_K:
-            model.addConstr(Q[j, k] >= Q[i, k] + q_j[j] + big_M * (X[i, j, k] - 1), 'c20')
+            model.addConstr(Q[j, k] >= Q[i, k] + q_j[j] + big_M * (X[i, j, k] - 1), 'c21')
 
-# c22
+# c23
 for i in set_N:
     for j in set_N:
         for k in set_K:
-            model.addConstr(L[j, k] >= L[i, k] + t_i_j[i, j] + big_M * (X[i, j, k] - 1) + service_time, 'c22')
+            model.addConstr(L[j, k] >= L[i, k] + t_i_j[i, j] + big_M * (X[i, j, k] - 1) + service_time[j], 'c23')
 
 # model.setParam('TimeLimit', 200)
 model.optimize()
